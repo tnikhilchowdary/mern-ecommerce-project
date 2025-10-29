448cab31739aba86036feb76d9ef6ed182727ed2
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-10-29 08:11:40 -0500

    Server Setup

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+    res.send("Backend is running");
+})
+
+app.listen(PORT, () => {
+    console.log("Server is running");
+})
